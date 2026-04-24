77cb98d24a41cbe9619ee1fe353c7fe1bb08f780
sammysam254 <sammyseth260@gmail.com>
2026-04-24 08:58:09 +0300

    fix: merge conflicting migrations 0003 and 0004

---
+++ vertext_app/migrations/0005_merge.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('vertext_app', '0003_verification'),
+        ('vertext_app', '0004_adlink_thumbnail_nullable'),
+    ]
+
+    operations = [
+    ]
