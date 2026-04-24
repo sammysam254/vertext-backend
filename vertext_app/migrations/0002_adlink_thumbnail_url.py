5f26677b98767469544aee6509def12b1336dfb8
sammysam254 <sammyseth260@gmail.com>
2026-04-24 07:39:05 +0300

    fix: add migration to add thumbnail_url column to AdLink table

---
+++ vertext_app/migrations/0002_adlink_thumbnail_url.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('vertext_app', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='adlink',
+            name='thumbnail_url',
+            field=models.URLField(blank=True, max_length=500),
+        ),
+    ]
